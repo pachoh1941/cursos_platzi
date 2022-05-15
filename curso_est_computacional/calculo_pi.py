7c6fa9fbb43874b2c79b8e578b194ed97b715f3c
Juan F. Hurtado <jf.hurtado10@uniandes.edu.co>
2022-05-15 13:17:25 -0500

    Se modifica el calculo_pi_py para que grafique cuando es 1 intento. No está funcionando el script, revisar

--- curso_est_computacional/calculo_pi.py
+++ curso_est_computacional/calculo_pi.py
@@ -1,9 +1,12 @@
 import random
 import math
 from estadisticas import desviacion_estandar, media
+from bokeh.plotting import figure, show#, output_file
 
-def aventar_agujas(numero_de_agujas):
+def aventar_agujas(numero_de_agujas, numero_de_intentos):
     adentro_del_circulo = 0
+    x_vals = []
+    y_vals = []
 
     for _ in  range(numero_de_agujas):
         x = random.random() * random.choice([-1,1])
@@ -12,13 +15,21 @@ def aventar_agujas(numero_de_agujas):
 
         if distancia_desde_el_centro <= 1:
             adentro_del_circulo += 1
+            x_vals.append(x)
+            y_vals.append(y)
+
+    #Gráfica PI
+    if numero_de_intentos == 1:
+        fig = figure()
+        fig.scatter(x_vals, y_vals)
+        show(fig)
 
     return (4 * adentro_del_circulo) / numero_de_agujas
 
 def estimacion(numero_de_agujas, numero_de_intentos):
     estimados = []
     for _ in range(numero_de_intentos):
-        estimacion_pi = aventar_agujas(numero_de_agujas)
+        estimacion_pi = aventar_agujas(numero_de_agujas, numero_de_intentos)
         estimados.append(estimacion_pi)
 
     media_estimados = media(estimados)
@@ -39,4 +50,5 @@ def estimar_pi(precision, numero_de_intentos):
 
 
 if __name__=='__main___':
-    estimar_pi(0.01, 1000)
+    numero_de_intentos = int(input('Ingresa el número de intentos: '))
+    estimar_pi(0.01, numero_de_intentos)
